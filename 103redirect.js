d05872d49eb861f1d4c5b4acc1a5ade9b9734dbb
Gugo-le <guhyunseung0718@gmail.com>
2022-11-14 10:32:46 +0900

    103zoom

---
+++ 103redirect.js
@@ -0,0 +1,324 @@
+function zojong() //조종례: Zoom만 시행하는 과목
+{
+    swal("조, 종례: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://zoom.us/j/3585993000?pwd=SmVielBWUW9DNkVnekVmNERPYjBVZz09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function ca() //창체: Zoom만 시행하는 과목
+{
+    swal("창체: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://zoom.us/j/3585993000?pwd=SmVielBWUW9DNkVnekVmNERPYjBVZz09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function social() //통합사회: Zoom만 시행하는 과목
+{
+    swal("통합사회: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us04web.zoom.us/j/6315897692?pwd=UXo2eGdOaFYwQnNmYjhmMXVsTFJ6QT09";
+                    break;
+                default:
+
+            }
+        });
+
+}
+
+function history() //한국사: Zoom만 시행하는 과목
+{
+    swal("한국사: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us05web.zoom.us/j/2877750908?pwd=Vnl6ckhVUGduNXFoTnNBbWFpTFB6QT09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function math() //수학: Zoom만 시행하는 과목
+{
+    swal("수학: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us02web.zoom.us/j/9076717845?pwd=QVZkUmhTYWZaaGZrcEI3em5KVUtHZz09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function music() //음악: Zoom만 시행하는 과목
+{
+    swal("음악: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us02web.zoom.us/j/9222655961?pwd=RFhjVFl1T3FVVmN0aEhWOWs1MThVZz09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+
+function science() //통과: Zoom만 시행하는 과목
+{
+    swal("통합과학: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us04web.zoom.us/j/4313387961?pwd=d1pOQ3krOFJVWTc1dk1ER1p4blpYUT09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+
+function tamsil() //과탐: Zoom만 시행하는 과목
+{
+    swal("과학 탐구 실험: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://zoom.us/j/2987563875?pwd=NnlpTHVkZG1RT0wvSzdRWU1ERzAzUT09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function koreanpark() //국어 박병두: Zoom만 시행하는 과목
+{
+    swal("국어(박병두): Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us02web.zoom.us/j/7297277073?pwd=dFBlUU5CWG1Bb0dZeEEwZzZkUm8zdz09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function koreanahn() //국어 안지영: Zoom만 시행하는 과목
+{
+    swal("국어(안지영): Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://zoom.us/j/96401872426?pwd=bG5vaGc1a0s5eVVURDNrREJYcEFoUT09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function koreanjang() //국어 장기형: Zoom만 시행하는 과목
+{
+    swal("국어(장기형): Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://zoom.us/j/9782997894?pwd=Z3BDUXNiS1pOeVRFZHVCOHA1KzlKZz09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function tech() //기술: Zoom만 시행하는 과목
+{
+    swal("기술: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us05web.zoom.us/j/9420834821?pwd=QXlwbXJUOHplSXoyWUVpZHkvQzUyQT09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+
+function home() //가정: Zoom만 시행하는 과목
+{
+    swal("가정: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us02web.zoom.us/j/9407240816?pwd=MHMvMGFFY20wMDFORHN1T0NyLzhnUT09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+
+function english() //영어: Zoom만 시행하는 과목
+{
+    swal("영어: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://zoom.us/j/3585993000?pwd=SmVielBWUW9DNkVnekVmNERPYjBVZz09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function zinro() //진로: Zoom만 시행하는 과목
+{
+    swal("진로: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us02web.zoom.us/j/9061893383?pwd=aVo0MlRncXprSkdTNG5KeUtVUmdLQT09";
+                    break;
+                default:
+
+            }
+        });
+}
+
+function pe() //체육: Zoom만 시행하는 과목
+{
+    swal("체육: Zoom으로 이동할까요?", "", "success", {
+            buttons: {
+                cancel: "취소",
+                OK: true,
+            }
+        })
+        .then((value) => {
+            switch (value) {
+
+                case "OK":
+                    location.href = "https://us05web.zoom.us/j/5729925096?pwd=YnF4MTJyTm5HcGZzTnpSa1lqbHMvQT09";
+                    break;
+                default:
+
+            }
+        });
+}
---
+++ ddy.js
@@ -0,0 +1,28 @@
+ var v = new Date().getFullYear(),
+     z = new Date("Dec 12, " + v).getTime(),
+     x = setInterval(function() {
+         var f = new Date().getTime(),
+             g = z - f,
+             y = g / 864e5,
+             a = Math.floor(g / 864e5),
+             b = Math.floor(g % 864e5 / 36e5),
+             c = Math.floor(g % 36e5 / 6e4),
+             d = Math.floor(g % 6e4 / 1e3),
+             zz = v,
+             aa = new Date("Dec 12, " + zz).getTime(),
+             bb = aa - f,
+             cc = bb / 864e5,
+             dd = Math.floor(bb / 864e5),
+             ee = Math.floor(bb % 864e5 / 36e5),
+             ff = Math.floor(bb % 36e5 / 6e4),
+             gg = Math.floor(bb % 6e4 / 1e3);
+         if (y <= -1) {
+             document.getElementById("dday").innerText = `시험 ${dd}일`
+         } else {
+             if (0 >= g) {
+                 clearInterval(x), document.getElementById("dday").innerHTML = "final exam!", document.getElementById("dday").classList.add("dday")
+             } else {
+                 document.getElementById("dday").innerText = `시험 ${a}일`
+             }
+         }
+     }, 500)
